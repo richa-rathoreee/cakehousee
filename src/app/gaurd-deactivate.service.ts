7529a47d0fddac8f0403c25f8c86623063422248
Richa Rathore <richa.rathore.walkingtree.tech>
2021-05-18 23:08:41 +0530

    initial commit

--- src/app/gaurd-deactivate.service.ts
+++ src/app/gaurd-deactivate.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanDeactivate } from '@angular/router';
-
+import { PaymentComponent } from 'cakebakery/src/app/payment/payment.component';
 export interface CanComponentDeactivate {
   canDeactivate: () => boolean;
 }
